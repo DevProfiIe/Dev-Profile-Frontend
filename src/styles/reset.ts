3e706ac6c57ab7a0746ded874c857c4bba8d4b37
heondeam <aoao1023@gmail.com>
2023-07-18 14:11:21 +0900

    style: reset.css 설정

---
+++ src/styles/reset.ts
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+
+const resetStyle = css`
+  ${emotionReset}
+
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+    color: inherit;
+  }
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+    flex-shrink: 0;
+  }
+  :root {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    cursor: default;
+    line-height: 1.5;
+    overflow-wrap: break-word;
+    -moz-tab-size: 4;
+    tab-size: 4;
+  }
+  html,
+  body {
+    height: 100%;
+    font-size: 16px;
+  }
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
+  a {
+    text-decoration: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
+export default resetStyle;
