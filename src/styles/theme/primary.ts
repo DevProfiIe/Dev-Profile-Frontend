6d330fac4ea24d1b4c1db786dba1828bbe011f03
heondeam <aoao1023@gmail.com>
2023-07-25 01:48:47 +0900

    docs: primary 테마 파일 추가
    - 스타일 관련 primary 테마 파일 추가

---
+++ src/styles/theme/primary.ts
@@ -0,0 +1,17 @@
+const primary = {
+  color: {
+    white: '#FFFFFF',
+    merino: '#FBF8F4',
+    black: '#000000',
+    mintGreen: '#7AFF9E',
+    aquaMarine: '#7affb4',
+    blueCharcoal: '#01051B',
+  },
+
+  font: {
+    fontFamily: "'Roboto Mono', monospace",
+  },
+};
+
+export const { font, color } = primary;
+export default primary;
