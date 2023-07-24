b0f2aff1fb70a0299b9c27b8be45a21423c2b44d
heondeam <aoao1023@gmail.com>
2023-07-24 20:52:59 +0900

    feat: 메인 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
+/** @jsxImportSource @emotion/react */
+
+/* Libraries & Hooks */
 import { Route, Routes } from 'react-router-dom';
+import { Global } from '@emotion/react';
 
 /* Pages */
-import Search from './pages/search/Search';
-import Login from './pages/user/Login';
-import Join from './pages/user/Join';
+import Home from './pages/home/Home';
+import SignIn from './pages/signIn/SignIn';
+import Board from './pages/board/Board';
+import Resume from './pages/resume/Resume';
 
-function App() {
+/* Components */
+import PrivateRoute from './components/utils/PrivateRoute';
+import resetStyle from './styles/reset';
+import Layout from './pages/layout/Layout';
+import AuthCallback from './components/utils/AuthCallback';
+import Modal from './components/modal/Modal';
+import { useAppDispatch, useAppSelector } from './redux/store';
+
+const App: React.FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const isOpen = useAppSelector((state) => state.modal.isOpen);
+
   return (
-    <Routes>
-      <Route element={<Search />} path='/search' />
-    </Routes>
+    <>
+      <Global styles={resetStyle} />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+
+          {/* Private Route */}
+          <Route element={<PrivateRoute />}>
+            <Route path='board' element={<Board />} />
+          </Route>
+
+          <Route path='resume' element={<Resume />} />
+        </Route>
+        <Route path='auth/sign-in' element={<SignIn />} />
+        <Route path='auth/callback' element={<AuthCallback />} />
+      </Routes>
+      {isOpen && <Modal width='25rem' height='25rem' />}
+    </>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+/* Libraries */
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { CookiesProvider } from 'react-cookie';
+
+/* Components */
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Provider store={store}>
+    <CookiesProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CookiesProvider>
+  </Provider>,
+  // </React.StrictMode>,
 );
