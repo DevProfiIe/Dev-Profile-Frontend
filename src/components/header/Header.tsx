1ec6a940f76059b82038cc1ed6283718ead3236d
heondeam <aoao1023@gmail.com>
2023-07-24 20:46:55 +0900

    feat: header 컴포넌트 생성
    - header 컴포넌트 생성
    - 스타일 적용

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,39 @@
+/** @jsxImportSource @emotion/react */
+
+/* Libraries & Hooks */
+import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getCookie } from '~/utils/cookie';
+import { HeaderWrapper, HerderFont } from './header.styles';
+
+const Header: React.FC = (): JSX.Element => {
+  const token = getCookie('token');
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  /**
+   *
+   */
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateScroll);
+  }, []);
+
+  return (
+    <HeaderWrapper scrollPosition={scrollPosition}>
+      <h1
+        css={css`
+          ${HerderFont}
+        `}
+      >
+        TEAM 5
+      </h1>
+      <div>{token ? '로그아웃 ' : <Link to='/auth/sign-in'>로그인</Link>}</div>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/header/header.styles.ts
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const HeaderWrapper = styled.div<{ scrollPosition: number }>`
+  width: ${({ scrollPosition }) => (scrollPosition > 50 ? '85%' : '80%')};
+  height: 4rem;
+  box-shadow: ${({ scrollPosition }) =>
+    scrollPosition > 50 ? '0.3rem 0.3rem 0.6rem rgba(0, 0, 0, 0.1)' : 'none'};
+  background-color: ${({ scrollPosition }) => (scrollPosition > 50 ? 'white' : 'none')};
+  border-radius: ${({ scrollPosition }) => (scrollPosition > 50 ? '5rem' : '0')};
+  top: 1.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  transition: 0.3s;
+  padding: 0 1.3rem;
+  position: fixed;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 5;
+`;
+
+export const HerderFont = css`
+  font-family: 'Bungee Shade', cursive;
+  font-weight: normal;
+  font-size: 2rem;
+  background: linear-gradient(
+    -45deg,
+    #143cfd 25%,
+    #4faae5 25%,
+    #1a4bfc 50%,
+    #1d4dfb 50%,
+    #0526ff 75%,
+    #0e34ff 75%,
+    #189bfa
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 20px 20px;
+  background-position: 0 0;
+  animation: stripes 1s linear infinite;
+  @keyframes stripes {
+    100% {
+      background-position: 20px 0, 20px 0, 20px 0;
+    }
+  }
+`;
