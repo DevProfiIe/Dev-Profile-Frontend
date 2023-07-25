36a504fe2d5c4697bd8e3a9cfeddd50655a7eb62
heondeam <aoao1023@gmail.com>
2023-07-26 00:00:19 +0900

    style: home 페이지 마크업 구조 변경

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,7 +10,16 @@ import Search from '~/components/search/Search';
 
 /* ETC */
 import line from '~/assets/images/line.png';
-import { BackgroundImage, HeightBox, HomeBox, HomeWrapper } from './home.styles';
+import {
+  BackgroundImage,
+  HeightBox,
+  HomeContentsBox,
+  HomeParagragh,
+  HomeSearchBox,
+  HomeSection,
+  HomeText,
+  HomeWrapper,
+} from './home.styles';
 
 const MAIN_TEXT = 'nd discover underlying insights from github.'.split('');
 
@@ -47,150 +56,136 @@ const Home: React.FC = (): JSX.Element => {
     <HomeWrapper>
       <BackgroundImage zIndex={2} img={background} />
       <BackgroundImage zIndex={1} img={line} />
-      <HomeBox>
-        <h1
-          css={css`
-            font-weight: 500;
-            font-size: 3rem;
-            line-height: 100%;
-          `}
-        >
-          DEV-PROFILE 🧙🏻‍♂️
-        </h1>
-        <HeightBox height={'2.25rem'} />
+      <HomeSection>
         <div
           css={css`
-            display: flex;
-            flex-flow: column nowrap;
-            align-items: center;
+            position: relative;
           `}
         >
-          <p
-            css={css`
-              font-weight: 500;
-              font-size: 1.25rem;
-              line-height: 134%;
-            `}
-          >
-            Just type in a box and our AI will instantly <br />
-            summarize key information, recall exact details,
-          </p>
-          <div
-            css={css`
-              position: relative;
-            `}
-          >
-            <div
+          <HomeSearchBox>
+            <h1
               css={css`
-                opacity: 0.16;
-                white-space: nowrap;
-                color: #01051b8f;
                 font-weight: 500;
-                font-size: 1.25rem;
-                line-height: 134%;
-                letter-spacing: -0.01em;
+                font-size: 3rem;
+                line-height: 100%;
               `}
             >
-              and discover underlying insights from github.
-            </div>
-            <div
-              css={css`
-                position: absolute;
-                top: 0;
-                left: 0;
-                pointer-events: none;
-                font-weight: 500;
-                font-size: 1.25rem;
-                line-height: 134%;
-                letter-spacing: -0.01em;
-              `}
-            >
-              <div ref={textArea}>a</div>
+              DEV-PROFILE 🧙🏻‍♂️
+            </h1>
+            <HeightBox height={'2.25rem'} />
+            <HomeParagragh>
+              <p>
+                Just type in a box and our AI will instantly <br />
+                summarize key information, recall exact details,
+              </p>
               <div
                 css={css`
                   position: relative;
-                  display: inline-block;
-                  margin-right: 2px;
                 `}
               >
+                <HomeText>and discover underlying insights from github.</HomeText>
                 <div
                   css={css`
                     position: absolute;
-                    transform: translateY(-150%);
                     top: 0;
-                    left: ${distance + 'px'};
+                    left: 0;
+                    pointer-events: none;
+                    font-weight: 500;
+                    font-size: 1.25rem;
+                    line-height: 134%;
+                    letter-spacing: -0.01em;
                   `}
                 >
+                  <div ref={textArea}>a</div>
                   <div
                     css={css`
-                      background: hsla(231, 100%, 53%, 1);
-                      width: 4px;
-                      height: 38px;
-                      border-bottom-left-radius: 8px;
-                      border-bottom-right-radius: 8px;
+                      position: relative;
+                      display: inline-block;
+                      margin-right: 2px;
                     `}
                   >
                     <div
                       css={css`
                         position: absolute;
+                        transform: translateY(-150%);
                         top: 0;
-                        right: unset;
-                        left: 0;
-                        transform: translateY(calc(-100% + 2px));
-                        user-select: none;
-                        color: #ffffff;
-                        background: hsla(231, 100%, 53%, 1);
-                        border-radius: 8px 20px 20px 0px;
-                        padding: 8px 12px 8px 8px;
-                        font-size: 20px;
-                        font-weight: 400;
-                        line-height: 100%;
-                        letter-spacing: -0.02em;
-                        display: flex;
-                        width: max-content;
-                        gap: 12px;
-
-                        &: before {
-                          content: '';
-                          position: absolute;
-                          z-index: 1;
-                          background-color: transparent;
-                          bottom: 0px;
-                          left: 4px;
-                          transform: translateY(100%);
-                          height: 50px;
-                          width: 16px;
-                          border-top-left-radius: 16px;
-                          box-shadow: 0 -16px 0 0 hsla(231, 100%, 53%, 1);
-                        }
+                        left: ${distance + 'px'};
                       `}
                     >
                       <div
                         css={css`
-                          flex-shrink: 0;
-                          position: relative;
-                          z-index: 2;
+                          background: hsla(231, 100%, 53%, 1);
+                          width: 4px;
+                          height: 38px;
+                          border-bottom-left-radius: 8px;
+                          border-bottom-right-radius: 8px;
                         `}
                       >
-                        DEV-PROFILE
-                      </div>
-                      <div
-                        css={css`
-                          transform: none;
-                        `}
-                      >
-                        🧙🏻‍♂️
+                        <div
+                          css={css`
+                            position: absolute;
+                            top: 0;
+                            right: unset;
+                            left: 0;
+                            transform: translateY(calc(-100% + 2px));
+                            user-select: none;
+                            color: #ffffff;
+                            background: hsla(231, 100%, 53%, 1);
+                            border-radius: 8px 20px 20px 0px;
+                            padding: 8px 12px 8px 8px;
+                            font-size: 20px;
+                            font-weight: 400;
+                            line-height: 100%;
+                            letter-spacing: -0.02em;
+                            display: flex;
+                            width: max-content;
+                            gap: 12px;
+
+                            &: before {
+                              content: '';
+                              position: absolute;
+                              z-index: 3;
+                              background-color: transparent;
+                              bottom: 0px;
+                              left: 4px;
+                              transform: translateY(100%);
+                              height: 50px;
+                              width: 16px;
+                              border-top-left-radius: 16px;
+                              box-shadow: 0 -16px 0 0 hsla(231, 100%, 53%, 1);
+                            }
+                          `}
+                        >
+                          <div
+                            css={css`
+                              flex-shrink: 0;
+                              position: relative;
+                              z-index: 3;
+                            `}
+                          >
+                            DEV-PROFILE
+                          </div>
+                          <div
+                            css={css`
+                              transform: none;
+                            `}
+                          >
+                            🧙🏻‍♂️
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </HomeParagragh>
+            <HeightBox height={'2.875rem'} />
+            <Search outputBox={false} redirectTo='/resume' />
+            <HeightBox height={'5rem'} />
+            <HomeContentsBox />
+          </HomeSearchBox>
         </div>
-        <HeightBox height={'2.875rem'} />
-        <Search outputBox={false} redirectTo='resume' />
-      </HomeBox>
+      </HomeSection>
     </HomeWrapper>
   );
 };
--- src/pages/home/home.styles.ts
+++ src/pages/home/home.styles.ts
@@ -1,28 +1,48 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { font } from '~/styles/theme/primary';
+import { color, font } from '~/styles/theme/primary';
+
+const DisplayFlexColumn = css`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
+
+const HomeFontStyles = css`
+  font-weight: 500;
+  font-size: 1.25rem;
+  line-height: 134%;
+`;
+
+const BoxShadow = css`
+  box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.14), 0px 41.7776px 33.4221px rgba(0, 0, 0, 0.10064),
+    0px 22.3363px 17.869px rgba(0, 0, 0, 0.083455), 0px 12.5216px 10.0172px rgba(0, 0, 0, 0.07),
+    0px 6.6501px 5.32008px rgba(0, 0, 0, 0.056545), 0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0393604);
+`;
+
+const BordarRadius = css``;
 
 export const HomeWrapper = styled.div`
   width: 100%;
   height: calc(100vh - 9.375rem);
   display: flex;
   flex-flow: column nowrap;
-  position: relative;
   font-family: ${font.fontFamily};
   font-weight: 500;
 `;
 
-export const HomeBox = styled.div`
+export const HomeSearchBox = styled.div`
+  position: absolute;
   width: 100%;
-  top: 8rem;
+  top: 3rem;
   left: 0;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
   padding: 0 7rem;
-  position: absolute;
-  z-index: 3;
   letter-spacing: -0.04em;
+  z-index: 3;
 `;
 
 export const BackgroundImage = styled.div<{ zIndex: number; img: string }>`
@@ -38,4 +58,37 @@ export const BackgroundImage = styled.div<{ zIndex: number; img: string }>`
 export const HeightBox = styled.div<{ height: string }>`
   width: 100%;
   height: ${({ height }) => height};
+`;
+
+export const HomeSection = styled.div`
+  width: 100%;
+  height: auto;
+`;
+
+export const HomeParagragh = styled.div`
+  ${DisplayFlexColumn}
+
+  p {
+    font-weight: 500;
+    font-size: 1.25rem;
+    line-height: 134%;
+  }
+`;
+
+export const HomeText = styled.div`
+  opacity: 0.16;
+  white-space: nowrap;
+  color: #01051b8f;
+  font-weight: 500;
+  font-size: 1.25rem;
+  line-height: 134%;
+  letter-spacing: -0.01em;
+`;
+
+export const HomeContentsBox = styled.div`
+  width: 840px;
+  height: 400px;
+  background-color: ${color.blueCharcoal};
+  border-radius: 40px;
+  ${BoxShadow};
 `;
