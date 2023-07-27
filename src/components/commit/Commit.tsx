808be7a4319d44844006d5511015b940c923940c
heondeam <aoao1023@gmail.com>
2023-07-27 12:57:52 +0900

    feat: commit search 컴포넌트 생성

---
+++ src/components/commit/Commit.tsx
@@ -0,0 +1,250 @@
+import { css } from '@emotion/react';
+import { SearchAlt } from 'emotion-icons/boxicons-regular';
+import { useState } from 'react';
+import { CommitContent, CommitContentTop, CommitSearchBox } from './commit.styles';
+import { Popup } from 'emotion-icons/entypo';
+import { useAppDispatch } from '~/redux/store';
+import { open } from '~/redux/features/popupSlice';
+
+const Commit = () => {
+  const [isShow, setIsShow] = useState(false);
+  const dispatch = useAppDispatch();
+
+  const showSearchInputHandler = () => {
+    setIsShow((state) => !state);
+  };
+
+  const popupToggleHandler = () => {
+    dispatch(open());
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          css={css`
+            width: ${isShow ? '10rem' : '0'};
+            border-bottom: 1px solid #000;
+            transition: 0.3s;
+            text-indent: 0.5rem;
+          `}
+          placeholder='Search Commits'
+          onBlur={showSearchInputHandler}
+        />
+        <button>
+          <SearchAlt height={30} onClick={showSearchInputHandler} />
+        </button>
+      </div>
+      {isShow && (
+        <CommitSearchBox>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              onClick={popupToggleHandler}
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+          <CommitContent>
+            <CommitContentTop>
+              <p>커밋명</p>
+              <div
+                css={css`
+                  display: flex;
+                  flex-flow: row nowrap;
+                  gap: 0 0.7rem;
+                  font-size: 0.8rem;
+                `}
+              >
+                <p>관련키워드</p>
+                <p>날짜</p>
+              </div>
+            </CommitContentTop>
+            <div
+              css={css`
+                padding: 0.5rem;
+                border-radius: 999px;
+                border: 1px solid #eee;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+              `}
+            >
+              <Popup height={20} />
+            </div>
+          </CommitContent>
+        </CommitSearchBox>
+      )}
+    </>
+  );
+};
+
+export default Commit;
---
+++ src/components/commit/commit.styles.ts
@@ -0,0 +1,38 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const BoxShadow = css`
+  box-shadow: 0px 109px 106px rgb(92 40 0 / 1%), 0px 45.5376px 44.2843px rgb(92 40 0 / 1%),
+    0px 24.3466px 23.6765px rgb(92 40 0 / 1%), 0px 13.6485px 13.2728px rgb(92 40 0 / 1%),
+    0px 7.24861px 7.04911px rgb(92 40 0 / 1%), 0px 3.01631px 2.93329px rgb(92 40 0 / 2%),
+    0 0 0 1px rgb(0 0 0 / 4%);
+`;
+
+export const CommitWrapper = styled.div``;
+
+export const CommitSearchBox = styled.div`
+  position: fixed;
+  top: 4.5rem;
+  width: 25rem;
+  max-height: 25rem;
+  background-color: white;
+  overflow: auto;
+  border-radius: 1.5rem;
+  ${BoxShadow}
+`;
+
+export const CommitContent = styled.div`
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.2rem;
+  border-bottom: 1px solid #eee;
+`;
+
+export const CommitContentTop = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1rem 0;
+`;
