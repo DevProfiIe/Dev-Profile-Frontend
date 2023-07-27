889c4e7647082c47425d30a428dfe940669352d7
heondeam <aoao1023@gmail.com>
2023-07-27 13:03:36 +0900

    style: primary 스타일 수정

--- src/styles/theme/primary.ts
+++ src/styles/theme/primary.ts
@@ -6,12 +6,19 @@ const primary = {
     mintGreen: '#7AFF9E',
     aquaMarine: '#7affb4',
     blueCharcoal: '#01051B',
+    myBlue: 'hsla(231, 100%, 53%, 1)',
+    keep: '#F7F1E9',
   },
 
   font: {
     fontFamily: "'Roboto Mono', monospace",
   },
+
+  boxShadow: `box-shadow: 0px 109px 106px rgb(92 40 0 / 1%), 0px 45.5376px 44.2843px rgb(92 40 0 / 1%),
+  0px 24.3466px 23.6765px rgb(92 40 0 / 1%), 0px 13.6485px 13.2728px rgb(92 40 0 / 1%),
+  0px 7.24861px 7.04911px rgb(92 40 0 / 1%), 0px 3.01631px 2.93329px rgb(92 40 0 / 2%),
+  0 0 0 1px rgb(0 0 0 / 4%)`,
 };
 
-export const { font, color } = primary;
+export const { font, color, boxShadow } = primary;
 export default primary;
