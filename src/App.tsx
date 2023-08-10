7180af102cf90bd319a524983a3cf3967d98ca1a
heondeam <aoao1023@gmail.com>
2023-08-10 21:17:41 +0900

    feat: sw feat 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@
 /* Libraries & Hooks */
 import { Route, Routes } from 'react-router-dom';
 import { Global } from '@emotion/react';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 /* Pages */
 import Home from '~/pages/home/Home';
@@ -12,15 +14,70 @@ import Gallary from './pages/gallary/Gallary';
 import User from './pages/user/User';
 
 /* Components */
-// import PrivateRoute from '~/components/utils/PrivateRoute';
+import PrivateRoute from '~/components/utils/PrivateRoute';
 import Layout from '~/pages/layout/Layout';
 import AuthCallback from '~/components/utils/AuthCallback';
 
 /* Styles */
 import resetStyle from '~/styles/reset';
-import PrivateRoute from './components/utils/PrivateRoute';
+import { useEffect } from 'react';
+import { usePostSubscribeSerberMutation } from './redux/api';
+
+const firebaseConfig = {
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: 'dev-profile-251ff.firebaseapp.com',
+  projectId: 'dev-profile-251ff',
+  storageBucket: 'dev-profile-251ff.appspot.com',
+  messagingSenderId: '719328905080',
+  appId: '1:719328905080:web:bd2b2d550036d99ceb6a8d',
+  measurementId: 'G-P22S2F7S83',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
 const App: React.FC = (): JSX.Element => {
+  const [subscribeFunc, { isSuccess }] = usePostSubscribeSerberMutation();
+
+  async function requestPermission() {
+    console.log('권한 요청 중...');
+
+    const permission = await Notification.requestPermission();
+    if (permission === 'denied') {
+      console.log('알림 권한 허용 안됨');
+      return;
+    }
+
+    console.log('알림 권한이 허용됨');
+
+    const token = await getToken(messaging, {
+      vapidKey: `${import.meta.env.VITE_VAPID_KEY}`,
+    });
+
+    if (token) {
+      subscribeFunc({
+        token: token,
+        username: 'dbscks97',
+      });
+
+      if (isSuccess) {
+        console.log('구독 성공');
+      }
+    } else {
+      console.log('Can not get Token');
+    }
+
+    onMessage(messaging, (payload) => {
+      console.log(payload.notification?.title);
+      console.log(payload.notification?.body);
+    });
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
     <>
       <Global styles={resetStyle} />
@@ -28,12 +85,11 @@ const App: React.FC = (): JSX.Element => {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
 
+          <Route path='resume/:keyword' element={<Resume />} />
+          <Route path='mypage' element={<User />} />
+          <Route path='gallary' element={<Gallary />} />
           {/* Private Route */}
-          <Route element={<PrivateRoute />}>
-            <Route path='gallary' element={<Gallary />} />
-            <Route path='resume/:keyword' element={<Resume />} />
-            <Route path='mypage' element={<User />} />
-          </Route>
+          <Route element={<PrivateRoute />}></Route>
         </Route>
 
         <Route path='auth/sign-in' element={<SignIn />} />
--- src/components/utils/AuthCallback.tsx
+++ src/components/utils/AuthCallback.tsx
@@ -1,30 +1,13 @@
 /* Libraries & Hooks */
 import queryString from 'query-string';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
-import { useAuthUserQuery, usePostSubscribeSerberMutation } from '~/redux/api';
+import { useAuthUserQuery } from '~/redux/api';
 import { getCookie, setCookie } from '~/utils/cookie';
-import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 /* Components */
 import Loader from '../loader/Loader';
 import { useAppDispatch } from '~/redux/store';
 import { showMessages } from '~/redux/features/popupSlice';
-import { UserGithubInfo } from '~/redux/api/types';
-
-const firebaseConfig = {
-  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
-  authDomain: 'dev-profile-251ff.firebaseapp.com',
-  projectId: 'dev-profile-251ff',
-  storageBucket: 'dev-profile-251ff.appspot.com',
-  messagingSenderId: '719328905080',
-  appId: '1:719328905080:web:bd2b2d550036d99ceb6a8d',
-  measurementId: 'G-P22S2F7S83',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
 
 const AuthCallback: React.FC = () => {
   const token = getCookie('token');
@@ -46,7 +29,7 @@ const AuthCallback: React.FC = () => {
   /**
    * Rtk Query 구조분해할당
    */
-  const userAuth = useAuthUserQuery(
+  const { isError, isLoading, isSuccess, data, error } = useAuthUserQuery(
     {
       code: parseCode(),
     },
@@ -55,12 +38,10 @@ const AuthCallback: React.FC = () => {
     },
   );
 
-  const userData = (userAuth.data?.data as UserGithubInfo) ?? {};
-
-  const [subscribeFunc, { isSuccess }] = usePostSubscribeSerberMutation();
+  const userData = data?.data ?? {};
 
   const setUserData = () => {
-    setCookie('token', userAuth.data?.token, {
+    setCookie('token', data?.token, {
       path: '/',
       sameSite: 'strict',
     });
@@ -68,64 +49,27 @@ const AuthCallback: React.FC = () => {
     localStorage.setItem('userInfo', JSON.stringify(userData));
   };
 
-  async function requestPermission() {
-    console.log('권한 요청 중...');
-
-    const permission = await Notification.requestPermission();
-    if (permission === 'denied') {
-      console.log('알림 권한 허용 안됨');
-      return;
-    }
-
-    console.log('알림 권한이 허용됨');
-
-    const token = await getToken(messaging, {
-      vapidKey: `${import.meta.env.VITE_VAPID_KEY}`,
-    });
-
-    if (token) {
-      subscribeFunc({
-        token: token,
-        username: 'dbscks97',
-      });
-
-      if (isSuccess) {
-        console.log('구독 성공');
-      }
-    } else {
-      console.log('Can not get Token');
-    }
-
-    onMessage(messaging, (payload) => {
-      console.log(payload.notification?.title);
-      console.log(payload.notification?.body);
-    });
-  }
-
   // if (isLoading) return;
   if (token) {
-    requestPermission();
     navigate('/');
   }
 
-  if (userAuth.isError) {
+  if (isError) {
     dispatch(
       showMessages({
-        msg: userAuth.error,
+        msg: error,
         content: 'DevProfile Login Error',
         type: 'error',
       }),
     );
   }
 
-  if (userAuth.isSuccess) {
+  if (isSuccess) {
     setUserData();
   }
   // if (isError) return <Message msg={JSON.stringify(error)} />;
 
-  return (
-    <>{userAuth.isLoading ? <Loader /> : userAuth.isSuccess ? <Navigate to='/' /> : <Loader />}</>
-  );
+  return <>{isLoading ? <Loader /> : isSuccess ? <Navigate to='/' /> : <Loader />}</>;
 };
 
 export default AuthCallback;
